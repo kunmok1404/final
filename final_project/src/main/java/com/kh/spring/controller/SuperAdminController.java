2b2bbecb28a80f3a0066f8f585fcbb75970d0e6e
Owner <Owner@CutyBoy>
2019-08-18 22:52:40 +0900

    8.18근목

--- final_project/src/main/java/com/kh/spring/controller/SuperAdminController.java
+++ final_project/src/main/java/com/kh/spring/controller/SuperAdminController.java
@@ -3,13 +3,14 @@ package com.kh.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// Admin관리자
+// 슈퍼관리자
 @Controller
 @RequestMapping("/admin")
-public class AdminController {
+public class SuperAdminController {
 
+	// 메인
 	@RequestMapping({"","/"})
 	public String home() {
-		return "admin/home";
+		return "admin/super/home";
 	}
 }
